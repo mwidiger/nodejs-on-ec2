6e161b0616047310dbd36ee084f50fa82bbc9ac1
Matt Widiger <matt.widiger@gmail.com>
2025-04-12 18:47:31 -0400

    server side redirects to https

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+var fs = require ('fs');
 var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('key.pem');
+var certificate = fs.readFileSync('cert.pem');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+app.use(function(req, res, next) {
+  res.redirect('https://' + req.headers.host + req.url);
+  next();
+});
+
+httpServer.listen(80);
+
+httpsServer.listen(443, () => {
+  console.log('HTTP Server running on port 80');
+});
